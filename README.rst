58157e7d36ad8eb160b6ed9b94ff73b20657ed0c
Tomas Poveda <tpovedatd@gmail.com>
2020-12-25 19:33:32 +0100

    Added base repository structure and files

---
+++ README.rst
@@ -0,0 +1,37 @@
+tpDcc-libs-options
+============================================================
+
+Customizable and easy to use data library.
+
+.. image:: https://img.shields.io/badge/Python-2.7-yellow?logo=python
+    :target: https://www.python.com/
+
+.. image:: https://img.shields.io/badge/Python-3.7-yellow?logo=python
+    :target: https://www.python.com/
+
+.. image:: https://img.shields.io/badge/Maya-2018-green?logo=autodesk
+    :target: https://www.autodesk.com/
+
+.. image:: https://img.shields.io/badge/Maya-2019-green?logo=autodesk
+    :target: https://www.autodesk.com/
+
+.. image:: https://img.shields.io/badge/Maya-2020-green?logo=autodesk
+    :target: https://www.autodesk.com/
+
+.. image:: https://travis-ci.com/tpDcc/tpDcc-libs-options.svg?branch=master&kill_cache=1
+    :target: https://travis-ci.com/tpDcc/tpDcc-libs-options
+
+.. image:: https://coveralls.io/repos/github/tpDcc/tpDcc-libs-options/badge.svg?branch=master&kill_cache=1
+    :target: https://coveralls.io/github/tpDcc/tpDcc-libs-options?branch=master
+
+.. image:: https://img.shields.io/badge/docs-sphinx-orange
+    :target: https://tpDcc.github.io/tpDcc-libs-options
+
+.. image:: https://img.shields.io/github/license/tpDcc/tpDcc-libs-options
+    :target: https://github.com/tpDcc/tpDcc-libs-options/blob/master/LICENSE
+
+.. image:: https://img.shields.io/pypi/v/tpDcc-libs-options?branch=master&kill_cache=1
+    :target: https://pypi.org/project/tpDcc-libs-options
+
+.. image:: https://img.shields.io/badge/code_style-pep8-blue
+    :target: https://www.python.org/dev/peps/pep-0008/
---
+++ tpDcc/libs/options/__init__.py
@@ -0,0 +1,61 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Initialization module for tpDcc-libs-options
+"""
+
+from __future__ import print_function, division, absolute_import
+
+import os
+import logging.config
+
+from tpDcc.core import library
+
+LIB_ID = 'tpDcc-libs-options'
+LIB_ENV = LIB_ID.replace('-', '_').upper()
+
+LOGGER = logging.getLogger('tpDcc-libs-options')
+
+
+class OptionsLib(library.DccLibrary, object):
+    def __init__(self, *args, **kwargs):
+        super(OptionsLib, self).__init__(*args, **kwargs)
+
+    @classmethod
+    def config_dict(cls, file_name=None):
+        base_tool_config = library.DccLibrary.config_dict(file_name=file_name)
+        tool_config = {
+            'name': 'Options Library',
+            'id': LIB_ID,
+            'supported_dccs': {'maya': ['2017', '2018', '2019', '2020']},
+            'tooltip': 'Library to easily create data driven options/attributes views.'
+        }
+        base_tool_config.update(tool_config)
+
+        return base_tool_config
+
+
+def create_logger(dev=False):
+    """
+    Creates logger for current tpDcc-libs-datalibrary package
+    """
+
+    logger_directory = os.path.normpath(os.path.join(os.path.expanduser('~'), 'tpDcc', 'logs', 'libs'))
+    if not os.path.isdir(logger_directory):
+        os.makedirs(logger_directory)
+
+    logging_config = os.path.normpath(os.path.join(os.path.dirname(__file__), '__logging__.ini'))
+
+    logging.config.fileConfig(logging_config, disable_existing_loggers=False)
+    logger = logging.getLogger('tpDcc-libs-options')
+    dev = os.getenv('TPDCC_DEV', dev)
+    if dev:
+        logger.setLevel(logging.DEBUG)
+        for handler in logger.handlers:
+            handler.setLevel(logging.DEBUG)
+
+    return logger
+
+
+create_logger()
